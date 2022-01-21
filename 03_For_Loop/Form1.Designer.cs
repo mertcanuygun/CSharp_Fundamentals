e471f5d2b6964695ca573d3e46ed91aee9480dea
Mertcan Uygun <mertcan.uygun@bilgeadamakademi.com>
2022-01-21 23:54:45 +0300

    For Loop Example

---
+++ 03_For_Loop/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace _03_For_Loop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(67, 55);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(160, 70);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(313, 55);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(249, 429);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(621, 55);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(247, 429);
+            this.listBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 726);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ 03_For_Loop/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03_For_Loop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            double odds = 0;
+            double evens = 0;
+
+            for (int i = 0; i <= 25; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evens = Math.Pow(i, 2) + evens;
+                    listBox2.Items.Add($"{i} x {i} = {(Math.Pow(i, 2))}");
+                }
+                else
+                {
+                    odds = Math.Pow(i, 2) + odds;
+                    listBox1.Items.Add($"{i} x {i} = {(Math.Pow(i, 2))}");
+                }
+            }
+            MessageBox.Show($"Sum of the squares of odd numbers: {odds}\n Sum of the squares of even numbers: {evens}");
+        }
+    }
+}
