dd9d6e28e8c1701e92cbcf37e6e9a43e4858ab83
Mertcan Uygun <mertcan.uygun@bilgeadamakademi.com>
2022-01-23 22:13:10 +0300

    Custom fucntion examples 1-Roll Dice

---
+++ 04_Custom Functions/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace _04_Custom_Functions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_RollDice = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.btn_RollDice);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(388, 345);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Roll Dice";
+            // 
+            // btn_RollDice
+            // 
+            this.btn_RollDice.Location = new System.Drawing.Point(155, 52);
+            this.btn_RollDice.Name = "btn_RollDice";
+            this.btn_RollDice.Size = new System.Drawing.Size(210, 254);
+            this.btn_RollDice.TabIndex = 0;
+            this.btn_RollDice.Text = "Roll Dice";
+            this.btn_RollDice.UseVisualStyleBackColor = true;
+            this.btn_RollDice.Click += new System.EventHandler(this.btn_RollDice_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(17, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(109, 254);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 614);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_RollDice;
+    }
+}
+
---
+++ 04_Custom Functions/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_Custom_Functions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Creating a function to generate random 2 numbers for dice and listing them in the listbox1.
+
+        string rollingdice()
+        {
+            Random random = new Random();
+            int[] dice = new int[2];
+
+
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = random.Next(1, 7);
+                listBox1.Items.Add(dice[i]);
+            }
+            return listBox1.Items.ToString();
+        }
+
+        //calling the function
+        private void btn_RollDice_Click(object sender, EventArgs e)
+        {
+            rollingdice();
+        }
+    }
+}
