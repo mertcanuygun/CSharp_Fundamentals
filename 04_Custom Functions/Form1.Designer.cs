df71695599fb8c2ab7fa36aecf7e2f36267260f7
Mertcan Uygun <mertcan.uygun@bilgeadamakademi.com>
2022-01-28 21:37:12 +0300

    Some improvements for RollDice game.

--- 04_Custom Functions/Form1.Designer.cs
+++ 04_Custom Functions/Form1.Designer.cs
@@ -29,40 +29,80 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btn_RollDice = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_RollDice = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_firstGuess = new System.Windows.Forms.TextBox();
+            this.txt_secondGuess = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.txt_secondGuess);
+            this.groupBox1.Controls.Add(this.txt_firstGuess);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.btn_RollDice);
+            this.groupBox1.Controls.Add(this.listBox1);
             this.groupBox1.Location = new System.Drawing.Point(13, 13);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(388, 345);
+            this.groupBox1.Size = new System.Drawing.Size(450, 356);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Roll Dice";
-            // 
-            // btn_RollDice
-            // 
-            this.btn_RollDice.Location = new System.Drawing.Point(155, 52);
-            this.btn_RollDice.Name = "btn_RollDice";
-            this.btn_RollDice.Size = new System.Drawing.Size(210, 254);
-            this.btn_RollDice.TabIndex = 0;
-            this.btn_RollDice.Text = "Roll Dice";
-            this.btn_RollDice.UseVisualStyleBackColor = true;
-            this.btn_RollDice.Click += new System.EventHandler(this.btn_RollDice_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(17, 52);
+            this.listBox1.Location = new System.Drawing.Point(305, 64);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(109, 254);
+            this.listBox1.Size = new System.Drawing.Size(106, 79);
             this.listBox1.TabIndex = 1;
+            // 
+            // btn_RollDice
+            // 
+            this.btn_RollDice.Location = new System.Drawing.Point(20, 165);
+            this.btn_RollDice.Name = "btn_RollDice";
+            this.btn_RollDice.Size = new System.Drawing.Size(391, 120);
+            this.btn_RollDice.TabIndex = 0;
+            this.btn_RollDice.Text = "Roll Dice";
+            this.btn_RollDice.UseVisualStyleBackColor = true;
+            this.btn_RollDice.Click += new System.EventHandler(this.btn_RollDice_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "First Guess";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Second Guess";
+            // 
+            // txt_firstGuess
+            // 
+            this.txt_firstGuess.Location = new System.Drawing.Point(174, 64);
+            this.txt_firstGuess.Name = "txt_firstGuess";
+            this.txt_firstGuess.Size = new System.Drawing.Size(115, 31);
+            this.txt_firstGuess.TabIndex = 4;
+            // 
+            // txt_secondGuess
+            // 
+            this.txt_secondGuess.Location = new System.Drawing.Point(174, 112);
+            this.txt_secondGuess.Name = "txt_secondGuess";
+            this.txt_secondGuess.Size = new System.Drawing.Size(115, 31);
+            this.txt_secondGuess.TabIndex = 5;
             // 
             // Form1
             // 
@@ -73,6 +113,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -82,6 +123,10 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button btn_RollDice;
+        private System.Windows.Forms.TextBox txt_secondGuess;
+        private System.Windows.Forms.TextBox txt_firstGuess;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- 04_Custom Functions/Form1.cs
+++ 04_Custom Functions/Form1.cs
@@ -17,9 +17,9 @@ namespace _04_Custom_Functions
             InitializeComponent();
         }
 
-        //Creating a function to generate random 2 numbers for dice and listing them in the listbox1.
+        //Creating a function to generate random 2 numbers for dice and listing them in the listbox1. Then sending a message whether winning or losing according to the number that have been given by the user.
 
-        string rollingdice()
+        string rollingdice(int guess1, int guess2)
         {
             Random random = new Random();
             int[] dice = new int[2];
@@ -30,13 +30,34 @@ namespace _04_Custom_Functions
                 dice[i] = random.Next(1, 7);
                 listBox1.Items.Add(dice[i]);
             }
+            if (guess1 > 7 || guess1 < 1 || guess2 > 7 || guess2 < 1)
+            {
+                listBox1.Visible = false;
+                MessageBox.Show("Please enter your guesses correctly!");
+            }
+            else
+            {
+                if((guess1 == dice[0] || guess1 == dice[1]) && (guess2 == dice[0] || guess2 == dice[1]))
+                {
+                    listBox1.Visible = true;                  
+                    MessageBox.Show("You WIN!!!");
+                }
+                else
+                {
+                    listBox1.Visible = true;
+                    MessageBox.Show("Opps! Try Again!");
+                }
+            }
             return listBox1.Items.ToString();
         }
 
         //calling the function
         private void btn_RollDice_Click(object sender, EventArgs e)
         {
-            rollingdice();
+            int first = Convert.ToInt32(txt_firstGuess.Text);
+            int second = Convert.ToInt32(txt_secondGuess.Text);
+            rollingdice(first,second);
+            listBox1.Items.Clear();
         }
     }
 }
