af2f8902bb572143b8ab44948cabc36825854d8c
Mertcan Uygun <mertcan.uygun@bilgeadamakademi.com>
2022-01-29 00:22:13 +0300

    Foreach practices

--- 05_ForEach_Loop/Form1.Designer.cs
+++ 05_ForEach_Loop/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_example2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(191, 49);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(124, 44);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_example2);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.btn_start);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(415, 351);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cities";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(23, 49);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(148, 254);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btn_example2
+            // 
+            this.btn_example2.Location = new System.Drawing.Point(191, 108);
+            this.btn_example2.Name = "btn_example2";
+            this.btn_example2.Size = new System.Drawing.Size(124, 44);
+            this.btn_example2.TabIndex = 2;
+            this.btn_example2.Text = "Example 2";
+            this.btn_example2.UseVisualStyleBackColor = true;
+            this.btn_example2.Click += new System.EventHandler(this.btn_example2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_example2;
     }
 }
 
--- 05_ForEach_Loop/Form1.cs
+++ 05_ForEach_Loop/Form1.cs
@@ -16,5 +16,39 @@ namespace _05_ForEach_Loop
         {
             InitializeComponent();
         }
+
+        string[] sehirler = { "İstanbul", "Londra", "Moskova", "Los Angeles", "Amsterdam", "Madrid", "Cape Town", "Kuala Lumpur", "Düsseldorf", "Prag" };
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            string name = string.Empty;
+            int longest = 0;
+            foreach (string sehir in sehirler)
+            {
+                if (sehir.Replace(" ","").Length > longest)
+                {
+                    longest = sehir.Replace(" ","").Length;
+                    name = sehir; 
+                }
+            }
+            listBox1.Items.AddRange(sehirler);
+            MessageBox.Show($"Longest city name is '{name}' with {longest} characters");
+        }
+
+        private void btn_example2_Click(object sender, EventArgs e)
+        {
+            string name = string.Empty;
+            int shortest = 50;
+            foreach (string sehir in sehirler)
+            {
+                if (sehir.Replace(" ", "").Length < shortest)
+                {
+                    shortest = sehir.Replace(" ", "").Length;
+                    name = sehir;
+                }
+            }
+            listBox1.Items.AddRange(sehirler);
+            MessageBox.Show($"Shortest city name is '{name}' with {shortest} characters");
+        }
     }
 }
